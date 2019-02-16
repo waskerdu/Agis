4ded4d29fa20bc276eb08ff6761083bb92d4436a
Ben <benjamin.schwab.nonspam@gmail.com>
2019-02-16 12:08:31 -0800

    messing around with a copy of the player1 script

--- Assets/scripts/Player2.cs
+++ Assets/scripts/Player2.cs
@@ -8,6 +8,7 @@ public class Player2 : MonoBehaviour
     public int playerNum = 0; // used to select input source, player color
     public float speed = 10.0f;
     public float jumpPower = 10.0f;
+    public Vector2 dashPower = new Vector2(30f,10f);
     public float dashSpeed = 10.0f;
     public float dashTime = 0.3f;
     public float dashClock = 0.0f; //greater than zero during player dash, overrides all movement
@@ -16,7 +17,7 @@ public class Player2 : MonoBehaviour
     bool grounded = false; //set to false every frame, reset to true by oncollsision stay
     Rigidbody2D rb;
     SpriteRenderer rend;
-    bool spriteFacingRight = true;
+    bool spriteFacingLeft = true;
     public bool canDash = false;
     Light lig;
     GameObject frame;
@@ -48,55 +49,8 @@ public class Player2 : MonoBehaviour
         rb.gravityScale = 4.0f;
         Vector2 vel = rb.velocity;
 
-
-        Dash();
-        // dashing
-        if (dashClock > 0.0f)
-        {
-            rb.gravityScale = 0.0f;
-            dashClock -= Time.deltaTime;
-            rb.velocity = vel;
-            lig.enabled = true;
-            return;
-        }
-        else
-        {
-            lig.enabled = false;
-            frame.transform.eulerAngles = Vector2.zero;
-        }
-        if(Input.GetButtonDown(fire) && canDash)
-        {
-            rb.gravityScale = 0.0f;
-            vel.x = Input.GetAxisRaw(horizontal);
-            if(vel.x == 0.0f)
-            {
-                if(spriteFacingRight){vel.x = -1.0f;}
-                else{vel.x = 1.0f;}
-            }
-            else
-            {
-                vel.x = vel.x / Mathf.Abs(vel.x);
-            }
-            vel.y = Input.GetAxisRaw(vertical);
-            if(vel.y == 0.0f)
-            {
-                vel.y = 1.0f;
-            }
-            else{
-                vel.y = vel.y / Mathf.Abs(vel.y);
-            }
-            vel = vel.normalized * dashSpeed;
-            Debug.Log(vel);
-            dashClock = dashTime;
-            transform.position += Vector3.up * 0.1f;
-            canDash = false;
-            rb.velocity = vel;
-            vel.x *= 3.0f;
-            float frameAngle = -Vector2.SignedAngle(Vector2.right, vel);
-            Debug.Log(frameAngle);
-            frame.transform.eulerAngles = Vector3.forward * (frameAngle);
-            return;
-        }
+        //dashing
+        if (Dash(vel)) return;
 
         // movement
         vel.x = Input.GetAxisRaw(horizontal) * speed;
@@ -122,18 +76,19 @@ public class Player2 : MonoBehaviour
         }
 
         // sprite stuff
-        if(vel.x < 0.0f){spriteFacingRight = true;}
-        if(vel.x > 0.0f){spriteFacingRight = false;}
-        rend.flipX = spriteFacingRight;
+        if(vel.x < 0.0f){spriteFacingLeft = true;}
+        if(vel.x > 0.0f){spriteFacingLeft = false;}
+        rend.flipX = spriteFacingLeft;
 
         // wrap up
         rb.velocity = vel;
         grounded = false;
     }
 
+    //returns true if dashing, false if not dashing
     private bool Dash(Vector2 vel)
     {
-        // dashing
+        // if we are in the middle of a dash
         if (dashClock > 0.0f)
         {
             rb.gravityScale = 0.0f;
@@ -147,40 +102,52 @@ public class Player2 : MonoBehaviour
             lig.enabled = false;
             frame.transform.eulerAngles = Vector2.zero;
         }
+
+        //if we are starting a dash this frame
         if (Input.GetButtonDown(fire) && canDash)
         {
+            canDash = false;
+            dashClock = dashTime;
             rb.gravityScale = 0.0f;
+
+
             vel.x = Input.GetAxisRaw(horizontal);
+            //no x defaults sprite direction
             if (vel.x == 0.0f)
             {
-                if (spriteFacingRight) { vel.x = -1.0f; }
+                if (spriteFacingLeft) { vel.x = -1.0f; }
                 else { vel.x = 1.0f; }
             }
-            else
+            else 
             {
-                vel.x = vel.x / Mathf.Abs(vel.x);
+                vel.x = vel.x / Mathf.Abs(vel.x);//sets vel.x to either 1 or -1 based on input
             }
+
+
             vel.y = Input.GetAxisRaw(vertical);
+            //no y defaults to up
             if (vel.y == 0.0f)
             {
                 vel.y = 1.0f;
             }
-            else
+            else 
             {
-                vel.y = vel.y / Mathf.Abs(vel.y);
+                vel.y = vel.y / Mathf.Abs(vel.y);// 1 or -1 y
             }
-            vel = vel.normalized * dashSpeed;
-            Debug.Log(vel);
-            dashClock = dashTime;
-            transform.position += Vector3.up * 0.1f;
-            canDash = false;
+            vel = vel.normalized;
+            vel.x *= dashPower.x;
+            vel.y *= dashPower.y;
+            
+            //transform.position += Vector3.up * 0.1f; //why? this breaks the physics engine
+            
             rb.velocity = vel;
-            vel.x *= 3.0f;
             float frameAngle = -Vector2.SignedAngle(Vector2.right, vel);
             Debug.Log(frameAngle);
-            frame.transform.eulerAngles = Vector3.forward * (frameAngle);
-            return;
+            frame.transform.eulerAngles = Vector3.forward * (frameAngle); //what is this doing??
+            return true;
         }
+
+        return false;
     }
 
     void OnCollisionStay2D(Collision2D collisionInfo)
