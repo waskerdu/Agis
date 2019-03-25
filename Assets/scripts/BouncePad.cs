74dcc67b8ef4558b1a0f708e7d8c90348bf7d5fe
Ben <benjamin.schwab.nonspam@gmail.com>
2019-03-25 12:22:45 -0700

    added a non functioning bounce pad added 2d collider to players

---
+++ Assets/scripts/BouncePad.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour
+{
+    [SerializeField]
+    public float BounceVal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var other = collision.gameObject;
+        Debug.Log("Object Type: " + other.GetType());
+        var rb = other.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.AddForce(Vector2.up * BounceVal);
+            Debug.Log("we be bouncin");
+        }
+        
+    }
+}
